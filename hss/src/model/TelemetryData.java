cf8dc0201721f893c8ce3813dbce067427c4faef
Jarrett Mead <jarrett_mead@comcast.net>
2017-04-09 17:03:34 -0400

    Implemented air temp field and methods in model.

--- hss/src/model/TelemetryData.java
+++ hss/src/model/TelemetryData.java
@@ -17,7 +17,25 @@
 package model;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 
 public class TelemetryData {
 
+	private final DoubleProperty air_temp;
+
+	public TelemetryData() {
+		air_temp = new SimpleDoubleProperty(0.0);
+	}
+
+	public Double getAirTemp() {
+		return air_temp.get();
+	}
+
+	public DoubleProperty getAirTempProperty() {
+		return air_temp;
+	}
+
+	public void setAirTemp(Double air_temp) {
+		this.air_temp.set(air_temp);
+	}
 }
